58137a3a2da3d09bc5e438fc41594e545e3abcfb
PTDTW <113230263+PTDTW@users.noreply.github.com>
2025-03-16 23:46:07 +0800

    Remove unnecessary blank line in layout.tsx

--- src/app/(website)/(home)/layout.tsx
+++ src/app/(website)/(home)/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import ProgressBarProvider from "@/components/ProgressBarProvider"; // 新增的 Client Component
 import SEO from "@/config/SEO.json";
 
-
 export const metadata: Metadata = {
   title: SEO.Index.title,
   description: SEO.Index.description,
